7123b32c0385ea9e775911484ae852bdbbc8ce4d
chaimakr <chaima050@gmail.com>
2022-01-21 22:21:26 +0100

    bar service crud

--- src/bar/bar.module.ts
+++ src/bar/bar.module.ts
@@ -3,11 +3,12 @@ import { BarController } from './bar.controller';
 import { BarService } from './bar.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PartyRepository } from './repositories/party.repository';
+import { BarRepository } from './repositories/bar.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([
     PartyRepository,
-
+    BarRepository
   ])],
   controllers: [BarController],
   providers: [BarService]
--- src/bar/bar.service.ts
+++ src/bar/bar.service.ts
@@ -1,4 +1,65 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Party } from './entities/party.entity';
+import { Repository } from 'typeorm';
+import { Bar } from './entities/bar.entity';
+import { Owner } from '../authentification/entities/owner.entity';
+import { BarRepository } from './repositories/bar.repository';
+import { PartyRepository } from './repositories/party.repository';
 
 @Injectable()
-export class BarService {}
+export class BarService {
+  constructor(
+    @InjectRepository(Party) private partyRepository: Repository<Party>,
+    @InjectRepository(Bar) private barRepository: Repository<Bar>,
+    @InjectRepository(BarRepository) private customBarRepository: BarRepository,
+    @InjectRepository(PartyRepository) private customPartyRepository: PartyRepository,
+
+  ) {}
+
+  //add bar
+  async addBar (barName :  string, imageUrl: string, owner : Owner) {
+    let bar = new Bar(barName, 0,owner, imageUrl);
+    this.barRepository.save(bar);
+    owner.bar=bar;
+    Owner.save(owner);
+    return;
+  }
+  //list all bars
+  async findAllBars():Promise<Bar[]>{
+    return this.customBarRepository.findAll();
+  }
+
+  //find a bar by its id
+  async findBarById(id: number):Promise<Bar>{
+    return this.barRepository.findOne(id);
+  }
+
+  //find all parties
+  async findAllParties():Promise<Party[]>{
+    return this.customPartyRepository.findAll();
+  }
+
+  //find party by id
+  async findPartyById(id: number): Promise<Party>{
+    return this.partyRepository.findOne(id);
+  }
+
+  //find parties of a specific bar
+  async getBarParties(idbar : number): Promise<Party[]>{
+    let bar = await  Bar.findOne(idbar);
+    return this.partyRepository.find({
+      'bar': bar
+    })
+  }
+
+  //remove bar
+  async removeBar(id: number){
+    return this.barRepository.softDelete(id);
+  }
+
+  // remove restaurant
+  async removeRestaurant(id:number){
+    return this.partyRepository.softDelete(id);
+  }
+}
