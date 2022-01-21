b0f7f949273485dd0568feb938f4a5c5aea4b475
chaimakr <chaima050@gmail.com>
2022-01-21 19:29:58 +0100

    bar party entity

---
+++ src/bar/entities/party.entity.ts
@@ -0,0 +1,55 @@
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { Bar } from "./bar.entity";
+
+
+@Entity()
+export class Party extends BaseEntity {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+
+  @Column()
+  description: string;
+
+
+  @Column()
+  price: number;
+
+
+  @Column()
+  imageUrl: string;
+
+
+
+  @ManyToOne(() => Bar, bar => bar.parties, { eager: true ,onDelete:'CASCADE'})
+  bar: Bar;
+
+
+
+  constructor(
+    name: string,
+    description:string,
+    rate:number,
+    price:number,
+    bar:Bar,
+    imageUrl:string,
+  ) {
+    super();
+    this.name=name;
+    this.description=description;
+    this.price=price;
+    this.bar=bar;
+    this.imageUrl=imageUrl;
+  }
+}
